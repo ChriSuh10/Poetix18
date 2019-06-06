ac6c7b52dc3dc049e3f44d86f8d40d2e8357df56
dineshp <dpalanisamy13@gmail.com>
2019-06-06 18:30:05 -0400

    sparse update

--- run_rhetorical_generation_multiline_sparse.py
+++ run_rhetorical_generation_multiline_sparse.py
@@ -182,6 +182,7 @@ def process_gen(e,d):
         print("%d/%d\n" % (len(filtered), i))
         try:
             f_sparse.write("%d/%d\n" % (len(filtered), i))
+            f_verbose.write("%d/%d\n" % (len(filtered), i))
             gen_lines = lg.gen_line_gpt_rep_multiline(v, search_space=n, top_sent=top_sent)
             v.set_gen_lines(gen_lines)
             f_verbose.write(v.to_string())
