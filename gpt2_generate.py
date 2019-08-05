e6a597ab0b1830a05ed7160e53afc85ae2cef134
Isaac Zhang <xz162@cs.duke.edu>
2019-08-05 06:21:49 -0400

    updated gpt2 generate

--- gpt2_generate.py
+++ gpt2_generate.py
@@ -2,14 +2,15 @@ from py_files.Limericks import Limerick_Generate
 import argparse
 parser = argparse.ArgumentParser(description='This file is used to test limerick production.')
 parser.add_argument("seed", help="seed word used to start poem generation")
+parser.add_argument("seed2", help="second seed word")
 parser.add_argument("search_space", help="search space for the GPT2 model method",
                     type=int)
 
 args = parser.parse_args()
 lg = Limerick_Generate(model_name='345M',load_poetic_vectors=False)
-poem = lg.gen_poem_gpt(args.seed, args.seed,
-       prompt_length=100, search_space=args.search_space, story_line=True,
+poem = lg.gen_poem_gpt(args.seed, args.seed2,
+       prompt_length=30, search_space=args.search_space,
        enforce_syllables = True, enforce_stress = True,
-       search_space_coef=[1,1,0.9,0.9])
+       search_space_coef=[1,1,0.9,0.8])
 for line in poem:
     print(' '.join(line))
