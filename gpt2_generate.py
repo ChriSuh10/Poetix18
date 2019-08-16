db41efa66f0f586736b8dc0e1ef04d6cad305266
Xiaoxuan Zhang <zhangxiaoxuan@yahoo.com>
2019-08-16 17:29:31 +0800

    manually added templates

--- gpt2_generate.py
+++ gpt2_generate.py
@@ -9,7 +9,13 @@ parser.add_argument("search_space", help="search space for the GPT2 model method
 args = parser.parse_args()
 lg = Limerick_Generate(model_name='345M',load_poetic_vectors=False)
 poem = lg.gen_poem_gpt(args.seed, args.seed2,
-       prompt_length=30, search_space=args.search_space,
+       default_templates=
+       [['WP$', 'NN', 'VBD', 'JJR', 'THAN', 'NN'],
+        ['PDT', 'DT', 'NNS', 'VBD', 'IN'],
+        ['CC', 'DT', 'NN', 'MD', 'VB', 'VBN'],
+        ['SO', 'PRP', 'VBD', 'TO', 'NN']
+       ],
+       prompt_length=0, search_space=args.search_space,
        enforce_syllables = True, enforce_stress = True,
        search_space_coef=[1,1,0.9,0.8])
 for line in poem:
