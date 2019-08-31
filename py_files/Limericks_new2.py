26c754c954bbad0b0c3ce28b9b07d41547f76523
Jianyou Wang <jianyou.wang@duke.edu>
2019-08-31 13:22:27 -0400

    new method that includes multi template, storyline sets, and multi syllabi distribution

--- py_files/Limericks_new2.py
+++ py_files/Limericks_new2.py
@@ -245,7 +245,7 @@ class Limerick_Generate_new(Limerick_Generate):
 						# end_flag is the (POS, Sylls) of word if word can be the last_word for a template, False if not
 						# continue_flag is (POS,Sylls) if word can be in a template and is not the last word. False if not
 						debug=False
-						if iteration==5: debug=True
+						if iteratiton==8: debug=True
 						continue_flag=self.template_sylls_checking(pos_set=pos_set,sylls_set=sylls_set,template_curr=template_curr,num_sylls_curr=num_sylls_curr,possible=possible, num_sylls=num_sylls)
 						end_flag=self.end_template_checking(pos_set=pos_set,sylls_set=sylls_set,template_curr=template_curr,num_sylls_curr=num_sylls_curr,possible=possible, num_sylls=num_sylls, debug=debug)
 						if break_point_continue>=thresh_hold: continue_flag=False
@@ -289,6 +289,8 @@ class Limerick_Generate_new(Limerick_Generate):
 			print("========================= iteration {} ends ============================= \n".format(iteration))
 			sentences=self.diversity_sort(search_space,new_sentences)
 			print("{} sentences before diversity_sort, {} sentences afterwards, now {} finished_sentences".format(len(new_sentences),len(sentences), len(finished_sentences)))
+			for sen in sentences:
+				print(sen+"\n")
 		assert len(sentences)==0, "something wrong"
 		previous_data_temp=self.diversity_sort(search_space,finished_sentences)
 		previous_data=[(i[0],i[1],i[2]+["\n"],i[3]+i[4],i[6]) for i in previous_data_temp]
