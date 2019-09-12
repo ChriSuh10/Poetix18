f4c38dbd65e8b9ba9e844e8230a78762ee28b53b
Xiaoxuan Zhang <zhangxiaoxuan@yahoo.com>
2019-09-12 19:56:36 -0400

    refactored limericks2

--- run_Limericks_new2.py
+++ run_Limericks_new2.py
@@ -3,8 +3,8 @@ import nltk
 from py_files.Limericks_new2 import Limerick_Generate_new
 import fire
 import pdb
-def limericks_generation_gpt(model_name="345M",model_dir='gpt2/models/345M',search_space=100,prompt="life", thresh_hold=10):
+def limericks_generation_gpt(model_name="345M",model_dir='gpt2/models/345M',search_space=10,prompt="life"):
 	lg = Limerick_Generate_new()
-	lg.gen_poem_andre_new(prompt=prompt,search_space=search_space,thresh_hold=thresh_hold)
+	lg.gen_poem_andre_new(prompt=prompt,search_space=search_space)
 if __name__ == '__main__':
     fire.Fire(limericks_generation_gpt)
