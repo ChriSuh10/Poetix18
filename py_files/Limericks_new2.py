5009c9ddb6c3d474cf451631e4005274b13b9077
Jianyou Wang <jianyou.wang@duke.edu>
2019-09-13 07:24:45 -0400

    changes

--- py_files/Limericks_new2.py
+++ py_files/Limericks_new2.py
@@ -448,17 +448,17 @@ class Limerick_Generate_new(Limerick_Generate):
 			print("******************************** gpt2 Starts Processing Next Word **********************************")
 			logits = score_model(model_name=self.model_name, context_token = context_token)
 			print("******************************** gpt2 Finished Processing Next Word **********************************")
-			print("num of sentences considered {}".format(len(logits)))
 			logits_list, sentences_list= self.split_chunks(logits, sentences)
 			manager = mp.Manager()
 			output=manager.Queue()
 			processes = [mp.Process(target=self.batch_process_word, args=(which_line, possible,num_sylls,logits_list[mp_index], sentences_list[mp_index], output)) for mp_index in range(len(logits_list)) ]
+			print("******************************** multiprocessing starts with {} processes *************************************".format(len(processes)))
 			for p in processes:
 				p.start()
 
 			for p in processes:
 				p.join()
-
+			print("********************************** multiprocessing ends *****************************************************")
 			results = [output.get() for p in processes]
 			new_sentences, quasi_finished_sentences = [], []
 			for result in results:
