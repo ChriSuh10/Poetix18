9848c4805c017ce8d4a55b28d9853d42926fef54
Jianyou Wang <jianyou.wang@duke.edu>
2019-10-16 18:42:58 -0400

    changes

--- run_Limericks_new2.sh
+++ run_Limericks_new2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES="2" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "fish"
-CUDA_VISIBLE_DEVICES="2" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "fire"
-CUDA_VISIBLE_DEVICES="2" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "trip"
-CUDA_VISIBLE_DEVICES="2" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "water"
-CUDA_VISIBLE_DEVICES="2" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "love"
-CUDA_VISIBLE_DEVICES="2" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "sex"
+CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "planet"
+CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "fire"
+CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "animal"
+CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "kingdom"
+CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "love"
+CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 50 --retain_space 5 --prompt "sex"
