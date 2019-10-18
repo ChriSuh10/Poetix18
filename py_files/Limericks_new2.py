dda9df75c40f5b0edf14eefbdbbcdc6769e77e53
Jianyou Wang <jianyou.wang@duke.edu>
2019-10-18 05:29:34 -0400

    new method that includes multi template, storyline sets, and multi syllabi distribution

--- py_files/Limericks_new2.py
+++ py_files/Limericks_new2.py
@@ -160,7 +160,7 @@ class Limerick_Generate_new(Limerick_Generate):
 
 		assert len(w1s_rhyme_dict.keys()) > 0, "no storyline available"
 		last_word_dict=self.last_word_dict(w1s_rhyme_dict,w3s_rhyme_dict)
-		saved_directory="limericks_data_new_4/"
+		saved_directory="limericks_data_new_5/"
 		result_file_path = saved_directory + prompt+"_" + str(search_space)+"_"+str(retain_space)+".txt"
 		f = open(result_file_path,"a+")
 
