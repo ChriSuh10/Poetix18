f9ee18982da95d6d37d7d143c786cbb7af6ed8aa
Jianyou Wang <jianyou.wang@duke.edu>
2019-10-18 08:12:59 -0400

    new method that includes multi template, storyline sets, and multi syllabi distribution

--- py_files/Limericks_new2.py
+++ py_files/Limericks_new2.py
@@ -173,8 +173,8 @@ class Limerick_Generate_new(Limerick_Generate):
 			f.write(rhyme+":"+"\n")
 			f.write(" ".join(w1s_rhyme_dict[rhyme])+"\n")
 			'''
-			
-			text=random.choice(self.gen_first_line_new(rhyme.lower(),strict=True))
+			candidates=self.gen_first_line_new(rhyme.lower(),strict=True)
+			if len(candidates)>0: text=random.choice(candidates)
 			first_line_encodes = self.enc.encode(" ".join(text))
 			previous_data.append((tuple(first_line_encodes),(0,),tuple(text)+("\n",), (text[-1],"\n"),(rhyme,"")))
 
