f3ec35eac8e534e8f057a69a15efd2521ef2b454
Xiaoxuan Zhang <zhangxiaoxuan@yahoo.com>
2019-10-24 22:04:55 -0400

    changed prob threshold

--- run_Limericks_new2.py
+++ run_Limericks_new2.py
@@ -5,6 +5,6 @@ import fire
 import pdb
 def limericks_generation_gpt(model_name="345M",model_dir='gpt2/models/345M',search_space=50,prompt="life", retain_space=5, prob_threshold=-15):
 	lg = Limerick_Generate_new()
-	lg.gen_poem_andre_new(prompt=prompt,search_space=search_space, retain_space=retain_space)
+	lg.gen_poem_andre_new(prompt=prompt,search_space=search_space, retain_space=retain_space, prob_threshold=prob_threshold)
 if __name__ == '__main__':
     fire.Fire(limericks_generation_gpt)
