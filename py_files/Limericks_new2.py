4cb43e87aaf8553b72b442897d74036c6c8747c5
Jianyou Wang <jianyou.wang@duke.edu>
2019-10-26 22:33:41 -0400

    new method that includes multi template, storyline sets, and multi syllabi distribution

--- py_files/Limericks_new2.py
+++ py_files/Limericks_new2.py
@@ -751,8 +751,7 @@ class Limerick_Generate_new(Limerick_Generate):
 			# because they are usually one sentence
 			
 			if which_line == "fifth":
-				pdb.set_trace()
-				fourth_line_template = tuple("*".join(sentences[i][3]).split("\n")[-1].split("*"))[1:]
+				fourth_line_template = tuple("*".join(sentences[i][3]).split("\n")[-2].split("*"))[1:]
 				print("fourth_line_template : "+ str(fourth_line_template)+"\n")
 				possible = self.limerick_last_two_line_mapping[fourth_line_template]
 				print(possible)
