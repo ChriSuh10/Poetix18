ce6737ab97c613091926487d8591417f75de935e
Jianyou Wang <jianyou.wang@duke.edu>
2019-11-13 20:22:03 -0500

    reverse linkage between 4,5

--- run_Limericks.py
+++ run_Limericks.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import nltk
-from py_files.Limericks_new2 import Limerick_Generate_new
+from py_files.Limericks_original import Limerick_Generate_new
 import fire
 import pdb
 def limericks_generation_gpt(model_name="345M",model_dir='gpt2/models/345M',search_space=50,prompt="life", retain_space=5, prob_threshold=-10):
