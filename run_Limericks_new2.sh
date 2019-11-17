375c5ad4c90d9c23c318dd555400ce69d45178f2
Jianyou Wang <jianyou.wang@duke.edu>
2019-11-17 16:48:32 -0500

    running exp

--- run_Limericks_new2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 250 --retain_space 3 --prompt "planet"
-CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 250 --retain_space 3 --prompt "fire"
-CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 250 --retain_space 3 --prompt "animal"
-CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 250 --retain_space 3 --prompt "kingdom"
-CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 250 --retain_space 3 --prompt "love"
-CUDA_VISIBLE_DEVICES="3" python3 run_Limericks_new2.py --search_space 250 --retain_space 3 --prompt "sex"
