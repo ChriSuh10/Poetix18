c1d0eb0dc5dec21c624c7763079c704089538451
Jianyou Wang <jianyou.wang@duke.edu>
2019-11-30 14:11:20 -0500

    set threshold to None

--- run_Limericks.py
+++ run_Limericks.py
@@ -3,7 +3,7 @@ import nltk
 from py_files.Limericks_original import Limerick_Generate_new
 import fire
 import pdb
-def limericks_generation_gpt(model_name="345M",model_dir='gpt2/models/345M',search_space=50,prompt="life", retain_space=5, prob_threshold=-10, embedding=0.0):
+def limericks_generation_gpt(model_name="345M",model_dir='gpt2/models/345M',search_space=50,prompt="life", retain_space=5, prob_threshold=None, embedding=0.0):
 	lg = Limerick_Generate_new()
 	lg.gen_poem_andre_new(prompt=prompt,search_space=search_space, retain_space=retain_space, prob_threshold=prob_threshold, word_embedding_coefficient=embedding)
 if __name__ == '__main__':
