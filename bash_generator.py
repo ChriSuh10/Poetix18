55f940e3039db1a90dfe9a355257583d79649117
Jianyou Wang <jianyou.wang@duke.edu>
2019-12-03 22:17:18 -0500

    make bashfile no story no diversity

--- bash_generator.py
+++ bash_generator.py
@@ -1,15 +1,15 @@
 def bash_generator():
-	type="original"
-	saved_directory="final_testing_no_diversity"
+	type="no_story"
+	saved_directory="final_testing_no_diversity_no_story"
 	search_space=20
 	retain_space=30
 	word_embedding_coefficient=0.1
 	mode="multi"
 	diversity=False
-	cuda=1
+	cuda=2
 	prompt_list="blood, death, war, queen, happy, world, planet, fire, water, game, love, vegetable, fish, theater, tiger, library, fairy, duke, print, click"
 	prompt_list=prompt_list.split(", ")
-	with open("run_Limericks"+"_"+str(cuda)+".sh","w") as f:
+	with open("run_Limericks_"+saved_directory+"_"+str(cuda)+".sh","w") as f:
 		f.write("#!/bin/bash \n")
 		for prompt in prompt_list:
 			sentence="CUDA_VISIBLE_DEVICES="+str(cuda)+"   python3 run_Limericks.py"+" --type "+str(type)+" --saved_directory "+str(saved_directory)+" --mode "+str(mode)+" --diversity "+ str(diversity)+ " --cuda "+str(cuda)+" --word_embedding_coefficient "+str(word_embedding_coefficient)+" --search_space "+str(search_space)+" --retain_space "+str(retain_space)+" --prompt "+ str(prompt)+"\n"
