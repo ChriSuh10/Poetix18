c0853e75e301741629c6dc9d68cae24958ce996a
Jianyou Wang <jianyou.wang@duke.edu>
2019-12-05 01:46:41 -0500

    massive changes made to storyline

--- py_files/Limericks_original.py
+++ py_files/Limericks_original.py
@@ -720,7 +720,7 @@ class Limerick_Generate_new(Limerick_Generate):
 			print("******************************** gpt2 Starts Processing Next Word **********************************")
 			logits = score_model(model_name=self.model_name, context_token = context_token)
 			print("******************************** gpt2 Finished Processing Next Word **********************************")
-
+			'''
 			logits_list= self.split_chunks(logits)
 			sentences_list=self.split_chunks(sentences)
 			manager = mp.Manager()
@@ -738,8 +738,8 @@ class Limerick_Generate_new(Limerick_Generate):
 			for result in results:
 				new_sentences += result[0]
 				quasi_finished_sentences += result[1]
-
-			#new_sentences, quasi_finished_sentences= self.batch_process_word(which_line, possible, num_sylls, logits, sentences)
+			'''
+			new_sentences, quasi_finished_sentences= self.batch_process_word(which_line, possible, num_sylls, logits, sentences)
 			if self.punctuation[which_line]:
 				if len(quasi_finished_sentences)>0:
 					quasi_finished_sentences, diversity=self.diversity_sort(search_space,retain_space,quasi_finished_sentences, finished=True)
@@ -836,6 +836,7 @@ class Limerick_Generate_new(Limerick_Generate):
 			if which_line=="fourth":
 				rhyme_set_curr = self.w3s_rhyme_dict[sentences[i][6][1]]
 				rhyme_word=sentences[i][6][1]
+			pdb.set_trace()
 
 			# If it is the fifth line, the current template has to corresponds to the fourth line template
 			# because they are usually one sentence
@@ -940,5 +941,5 @@ class Limerick_Generate_new(Limerick_Generate):
 												sentences[i][6],
 												tuple_of_wema)
 									quasi_finished_sentences.append(word_tuple)
-		output.put((new_sentences, quasi_finished_sentences))
-		#return new_sentences, quasi_finished_sentences
+		#output.put((new_sentences, quasi_finished_sentences))
+		return new_sentences, quasi_finished_sentences
