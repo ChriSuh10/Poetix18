a13142f573dfc3af387bd894b5c581c6c29a56bb
Jianyou Wang <jianyou.wang@duke.edu>
2019-12-05 01:26:04 -0500

    massive changes made to storyline

--- py_files/Limericks.py
+++ py_files/Limericks.py
@@ -187,7 +187,7 @@ class Limerick_Generate:
         if 'JJ' in word_pos \
             or 'NN' in word_pos \
             or any('VB' in pos for pos in word_pos):
-            distances = [self.poetic_vectors.similarity(word, rhyme) for rhyme in rhyme_set if rhyme not in self.poetic_vectors]
+            distances = [self.poetic_vectors.similarity(word, rhyme) for rhyme in rhyme_set if rhyme in self.poetic_vectors]
             if len(distances) == 0:
                 return None
             return max(distances)
