7e4c54b66286a10b5cace30f06a22e3f6d9b69ff
Jianyou Wang <jianyou.wang@duke.edu>
2020-01-16 11:44:46 -0500

    changes

--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 48:00:00  # time requested in hour:minute:second
 # Request 1GB or RAM
 #SBATCH --gres=gpu:1
-#SBATCH --mem=30G --cpu-per-task=20
+#SBATCH --mem=30G --cpus-per-task=20
 #SBTACH -p compsci-gpu
 ##SBATCH -a 0-100 --ntasks-per-node=1
 #srun python3 run_Limericks.py --type no_story --sasved_directory 2020_Jan_DTS_no_story --mode multi --diversity True --word_embedding_coefficient 0.1 --search_space 25 --retain_space 30
