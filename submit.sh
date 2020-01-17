9f0b7d4fb2ff7b77b6f3a2add5ba171c13461ffb
Jianyou Wang <jianyou.wang@duke.edu>
2020-01-17 16:42:54 -0500

    changes

--- submit.sh
+++ submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=andre_test
+#SBATCH --job-name=nostory
 # Limit running time to 5 minutes.
 #SBATCH -t 48:00:00  # time requested in hour:minute:second
 # Request 1GB or RAM
 #SBATCH --gres=gpu:1
-#SBATCH --mem=100G --cpus-per-task=20
+#SBATCH --mem=300G --cpus-per-task=20
 #SBTACH -p compsci-gpu
 #SBATCH -a 0-100 --ntasks-per-node=1
 srun python3 run_Limericks.py -t "no_story" -dir "Jan_DTS_no_story" -m "multi" -div True -w 0.1 -ser 25 -re 30
--- submit_story.sh
+++ submit_story.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=andre_test
+#SBATCH --job-name=story
 # Limit running time to 5 minutes.
 #SBATCH -t 48:00:00  # time requested in hour:minute:second
 # Request 1GB or RAM
 #SBATCH --gres=gpu:1
-#SBATCH --mem=100G --cpus-per-task=20
+#SBATCH --mem=300G --cpus-per-task=20
 #SBTACH -p compsci-gpu
 #SBATCH -a 0-100 --ntasks-per-node=1
 srun python3 run_Limericks.py -t "original" -dir "Jan_DTS_story" -m "multi" -div True -w 0.1 -ser 25 -re 30
