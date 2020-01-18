c4c1e9afc7068ce72cedbbfd41fecdf016886a1c
Jianyou Wang <jianyou.wang@duke.edu>
2020-01-18 16:07:24 -0500

    changes

--- submit.sh
+++ submit.sh
@@ -3,6 +3,6 @@
 #SBATCH -t 48:00:00  
 #SBATCH --gres=gpu:1
 #SBATCH --mem=300G --cpus-per-task=20
-#SBTACH -p compsci-gpu --constraint=v100
+#SBTACH -p compsci-gpu --constraint=v100 --exclude=linux[1-50],gpu-compute[1-4]
 #SBATCH -a 0-19 --ntasks-per-node=1
 srun python3 run_Limericks.py -t "original" -dir "Jan_DTS_story" -m "multi" -div True -w 0.1 -ser 25 -re 30
