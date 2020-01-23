aa7aa5e34d9ea7b1f7afed2b539a27ef42751fe5
Jianyou Wang <jianyou.wang@duke.edu>
2020-01-23 13:37:06 -0500

    changes

--- submit_no_story_male.sh
+++ submit_no_story_male.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=no_story_male
 #SBATCH -t 48:00:00  
 #SBATCH --gres=gpu:1
-#SBATCH --mem=300G --cpus-per-task=20
-#SBATCH -p compsci-gpu 
-##--constraint=v100
+#SBATCH --mem=250G --cpus-per-task=20
+#SBATCH -p compsci-gpu --constraint=v100
 #SBATCH -a 0-100 --ntasks-per-node=1
 srun python3 run_Limericks.py -t "no_story" -dir "Feb_DTS_no_story" -m "multi" -div True -w 0.1 -ser 25 -re 30 -g "male"
