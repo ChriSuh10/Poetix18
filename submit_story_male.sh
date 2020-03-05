d36358af2d77fb4d673086124d1d4929670e025b
Jianyou Wang <jianyou.wang@duke.edu>
2020-03-05 14:19:49 -0500

    run more tests

--- submit_story_male.sh
+++ submit_story_male.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=story_male
+#SBATCH --job-name=multi_male
 #SBATCH -t 48:00:00  
 #SBATCH --gres=gpu:1
 #SBATCH --mem=300G --cpus-per-task=20
 #SBATCH -p compsci-gpu 
 ##--constraint=v100
-#SBATCH -a 0-100 --ntasks-per-node=1
-srun python3 run_Limericks.py -t "original" -dir "Feb_single_story" -m "single" -div True -w 0.1 -ser 25 -re 30 -g "male"
+#SBATCH -a 0-4 --ntasks-per-node=1
+srun python3 run_Limericks.py -t "original" -dir "Andre_story" -m "multi" -div True -w 0.1 -ser 25 -re 30 -g "male"
