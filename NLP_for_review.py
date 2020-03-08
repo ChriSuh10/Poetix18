d1d79b97bc48625fd4fa4eb1e13a34d47c3499d2
Jianyou Wang <jianyou.wang@duke.edu>
2020-03-08 17:11:58 -0400

    changes to review

--- NLP_for_review.py
+++ NLP_for_review.py
@@ -21,7 +21,7 @@ def softmax(x):
 	return ret
 
 def run():
-	with open("hair_dryer.pickle","rb") as f:
+	with open("py_files/hair_dryer.pickle","rb") as f:
 		data=pickle.load(f)
 	data_grouped=[]
 	for i in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]:
