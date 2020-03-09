4f2b341fd80ef8b685045320b41aa21c8a6abbab
Jianyou Wang <jianyou.wang@duke.edu>
2020-03-08 22:47:00 -0400

    changes

--- NLP_review.sh
+++ NLP_review.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=microwave
+#SBATCH --job-name=pacifier
 #SBATCH -t 48:00:00  
 #SBATCH --gres=gpu:1
 #SBATCH --mem=300G --cpus-per-task=20
 #SBATCH -p compsci-gpu 
 ##--constraint=v100
 #SBATCH -a 0 --ntasks-per-node=1
-srun python3 NLP_for_review.py -p "microwave"
+srun python3 NLP_for_review.py -p "pacifier"
